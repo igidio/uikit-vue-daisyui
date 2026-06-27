1c41fcdffa9711042a66f8ca8d0b05c8da5cde5a
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-06-27 13:35:25 -0400

    feat: add linkExactActiveClass to router and clean up Sidebar component formatting

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import MainLayout from '@/shared/layouts/main-layout/MainLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  linkExactActiveClass: 'text-base-100 bg-base-content',
   routes: [
     {
       path: '/',
--- src/shared/components/sidebar/Sidebar.vue
+++ src/shared/components/sidebar/Sidebar.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
-import { navigationOptions } from '@/shared/data/navigation-options'
+import { navigationOptions } from "@/shared/data/navigation-options";
 </script>
 
 <template>
-  <aside class="bg-base-200 max-h-full w-full overflow-y-auto border-r border-base-300">
-    <ul class="menu menu-sm p-4 w-full">
-      <li v-for="option in navigationOptions" :key="option.path">
-        <RouterLink
-          :to="'/' + option.path"
+	<aside
+		class="bg-base-200 max-h-full w-full overflow-y-auto border-r border-base-300"
+	>
+		<ul class="menu menu-sm p-4 w-full">
+			<li v-for="option in navigationOptions" :key="option.path">
+				<RouterLink 
+          :to="'/' + option.path" 
           :exact="option.path === ''"
         >
-          {{ option.label }}
-        </RouterLink>
-      </li>
-    </ul>
-  </aside>
+					{{ option.label }}
+				</RouterLink>
+			</li>
+		</ul>
+	</aside>
 </template>
+
+<!-- <style>
+@reference "tailwindcss";
+.router-link-exact-active {
+  @apply bg-black text-white dark:bg-white dark:text-black;
+}
+</style> -->
